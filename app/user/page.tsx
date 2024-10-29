0ec4c62d943c4938af081eeaac7ea539b8cdde23
chinmayck-etrillium <chinmayck24@gmail.com>
2024-10-29 12:47:27 +0530

    Added a new page

---
+++ app/user/page.tsx
@@ -0,0 +1,9 @@
+export default function UserPage() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center text-purple-950 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Hi Richybobish</h1>
+      </main>
+    </div>
+  );
+}
